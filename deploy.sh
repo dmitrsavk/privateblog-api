6146619b43ddbf003e1c3784211ba918766ee60f
d-savkin <d.savkin@qiwi.com>
2018-04-26 19:47:04 +0300

    asd

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 yarn
-forever stop /root/privateblog-api/src/server.js
-forever start /root/privateblog-api/src/server.js
+forever stop /root/privateblog-api/dist/server.js
+forever start /root/privateblog-api/dist/server.js
