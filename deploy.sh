174b0f9382af1641bcbce24a533c4d7245f405c4
d-savkin <d.savkin@qiwi.com>
2018-04-26 19:49:11 +0300

    asd

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 yarn
-forever stop /root/privateblog-api/dist/server.js
+babel ./src --presets es2017 --plugins transform-es2015-modules-commonjs -d ./dist
 forever start /root/privateblog-api/dist/server.js
